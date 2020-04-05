b603f5d0f9ad6f0395e9fc7a4f8cb23244ff77fc
Zetrax <zabjd22@gmail.com>
2020-04-06 00:23:30 +0300

    Added folder navigation Added Back functionality

--- src/view/script/filemanager.js
+++ src/view/script/filemanager.js
@@ -1,8 +1,14 @@
 const ipcRenderer = require("electron").ipcRenderer
+const path = require('path')
 
 // DOM Elements
 const foldersSection = document.getElementById('folder-section')
 const folderItem = document.getElementsByClassName('folder-item')
+const backBtn = document.getElementById('back-btn')
+
+// Data
+var currentDir = 'C:/'
+var previousDir = []
 
 ipcRenderer.on('async-reply', (event, reply) => {
     console.log(reply)
@@ -17,17 +23,17 @@ readFolderBtn.addEventListener('click', () => {
     console.log(folderPathTxt.value)
     clearFoldersSection()
     ipcRenderer.send('read-folder', folderPathTxt.value)
-    // const e = document.createElement('p')
-    // e.innerHTML = 'wait... reading folder'
-    // foldersSection.appendChild(e)
+    // TODO: add reload animation
+    // TODO: add validator for same current and previous folder 
+    //       If the same don't overwrite previousDir with currentDir
 })
 
 ipcRenderer.on('folder-details', (event, reply) => {
     clearFoldersSection()
-
+    
     let res = JSON.parse(reply)
     console.log(res)
-
+    
     console.log('List Folders')
     for (const folder of res.folders) {
         const element = document.createElement('a')
@@ -40,18 +46,38 @@ ipcRenderer.on('folder-details', (event, reply) => {
         element.setAttribute('href', '#')
         element.setAttribute('class', 'collection-item folder-item')
         foldersSection.appendChild(element)
+        // Adding Event for directory click
         element.addEventListener('click', (e) => {
+            /**
+             * TODO: save previousFolder
+             *       update currentFolder
+             *       remove folderItem list
+             *       list folder items
+             */
+            previousDir.push(currentDir)
+            currentDir = path.join(currentDir, dirName.innerText)
             e.preventDefault()
-            console.log(`changind dir: ${dirName.innerText}`)
+            ipcRenderer.send('read-folder', currentDir)
+            folderPathTxt.value = currentDir
+            console.log(`changind dir: ${currentDir}`)
         })
     } 
     console.log('List Files')
     for (const file of res.files) {
         const element = document.createElement('a')
-        element.innerText = `🗄 ${file}`
+        const icon = document.createElement('span')
+        const fileName = document.createElement('span')
+        fileName.innerText = ' ' + file
+        icon.innerText = '🗄 '
+        element.appendChild(icon)
+        element.appendChild(fileName)
         element.setAttribute('href', '#')
         element.setAttribute('class', 'collection-item folder-item')
         foldersSection.appendChild(element)
+        element.addEventListener('click', (e) => {
+            e.preventDefault()
+            console.log(`openning file: ${fileName.innerText}`)
+        })
     }
 })
 
@@ -70,4 +96,14 @@ const addEventToItems = () => {
         })
     }
 }
+
+backBtn.addEventListener('click', (e) => {
+    e.preventDefault()
+    if (previousDir.length == 0) return;
+
+    // heading back to previous directory
+    currentDir = previousDir.pop()
+    ipcRenderer.send('read-folder', currentDir)
+    folderPathTxt.value = currentDir
+})
 
